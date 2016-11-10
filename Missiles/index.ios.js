567c68f9890911321b5f169559701abe9fa76411
kevando <kevin@frameri.com>
2016-11-10 18:19:46 -0500

    Default rn meteor boiler plate, before switching shit over to firebase

---
+++ Missiles/index.ios.js
@@ -0,0 +1,28 @@
+import { AppRegistry } from 'react-native';
+import App from './src';
+
+AppRegistry.registerComponent('Missiles', () => App);
+
+
+
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// // Map example
+// // import App from './app/Maps';
+//
+// // v2 Missiles
+// import App from "./app";
+//
+// export default class Missiles extends Component {
+//   render() {
+//     return (<App />);
+//   }
+// }
+//
+// AppRegistry.registerComponent('Missiles', () => Missiles);
